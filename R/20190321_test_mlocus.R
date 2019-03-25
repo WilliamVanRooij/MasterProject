2a43b405b254cc77833d57f76469f708462a0a26
William van Rooij <williamvanrooij@gmail.com>
2019-03-25 10:25:06 +0100

    Work in progress.

--- R/20190321_test_mlocus.R
+++ R/20190321_test_mlocus.R
@@ -5,13 +5,13 @@ require(locus)
 
 i <- 1;
 
-n <- 200; p <- 100; p0 <- 10; d <- 100; d0 <- 80
+n <- 200; p <- 100; p0 <- 10; d <- 1; d0 <- 1
 
 gam_vb <- 0*c(1:p0);
 
 cor_type <- "autocorrelated"; 
 
-vec_rho <- runif(100, min = 0.25, max = 0.95)
+vec_rho <- runif(100, min = 0.5, max = 0.95)
 
 nb_cpus <- 4; # Unfortunately, the number of cores available on Windows is only 1. But when computing on another computer, one can change the number of cpus used.
 
@@ -20,12 +20,12 @@ seed <- 123; set.seed(seed);
 list_snps <- generate_snps(n, p, cor_type, vec_rho, n_cpus = nb_cpus,
                            user_seed = seed)
 
-list_phenos <- generate_phenos(n, d, cor_type = cor_type, vec_rho = vec_rho,
+list_phenos <- generate_phenos(n, d, cor_type = cor_type, vec_rho = vec_rho[1],
                                n_cpus = nb_cpus, user_seed = seed)
 
 dat_g <- generate_dependence(list_snps, list_phenos, ind_d0 = sample(1:d, d0),
                              ind_p0 = sample(1:p, p0), vec_prob_sh = 0.95,
-                             family = "gaussian", max_tot_pve = 0.5, block_phenos = TRUE, user_seed = seed)
+                             family = "gaussian", max_tot_pve = 0.9, block_phenos = TRUE, user_seed = seed)
 
 # vb_g <- locus(Y = dat_g$phenos, X=dat_g$snps, p0_av = p0, link = "identity", user_seed = seed)
 
@@ -40,9 +40,7 @@ mlocus <- function(fseed) {
   return(vb_g$gam_vb[,1])
 }
 
-mac = TRUE
-
-## WINDOWS Sys.info['sysname']
+mac = (Sys.info()['sysname'] != "Windows")
 
 if(!mac) {
   cores <- detectCores()
@@ -64,6 +62,6 @@ if(mac) {
   for(i in (1:10)){
       sum <- sum + m_vb_g[[i]]
   }
-  plot(sum/10)
+  plot(sum/10, main='Probabilities of link between a phenotype and SNPs',type='h',lwd=3,lend=1)
   
 }
