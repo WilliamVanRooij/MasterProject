9deb3f5b05b06422595d79a23671ecabd13054cb
William van Rooij <williamvanrooij@gmail.com>
2019-05-22 18:18:08 +0200

    Update 22.05.19

---
+++ R/annealing.R
@@ -0,0 +1,169 @@
+
+library(parallel)
+library(LaplacesDemon)
+
+require(MASS)
+require(echoseq)
+require(locus)
+require(R2OpenBUGS)
+require(coda)
+require(lattice)
+require(nor1mix)
+require(plotly)
+
+a <- 1
+
+rm(list= ls())
+
+RNGkind("L'Ecuyer-CMRG")
+set.seed(123);
+
+# ================================================================================================================================================ #
+#                                                                                                                                                  #
+#                                                                     TO DO                                                                        #
+#                                                                                                                                                  #
+# ================================================================================================================================================ #
+
+# Check the Windows part of the function 
+
+# ================================================================================================================================================ #
+#                                                                                                                                                  #
+#                                                                   VARIABLES                                                                      #
+#                                                                                                                                                  #
+# ================================================================================================================================================ #
+
+n <- 300; 
+p <- 500; p0 <- 5; 
+d <- 1; d0 <- 1
+
+
+iter <- 1
+
+
+for (k in sample(1:1e3,iter)){
+  
+  set.seed(k)  
+  
+  seed <-  k;
+  
+  cor_type <- "autocorrelated"; 
+  
+  vec_rho <- runif(floor(p/10), min = 0.98, max = 0.99)
+  #vec_rho <- c(0.98)
+  
+  nb_cpus <- 4;
+  
+  ind_d0 <-  sample(1:d, d0)
+  
+  ind_p0 <- c(3,13,17,23,43)
+  
+  p0_av <- 15
+  
+  user_seed <- sample(1:1e3, 100)
+  
+  vec_prob_sh <-  0.1 # proba that each SNP will be associated with another active phenotype
+  
+  max_tot_pve <-  0.8 # max proportion of phenotypic variance explained by the active SNPs
+  
+  list_snps <- generate_snps(n, p, cor_type, vec_rho, n_cpus = nb_cpus,
+                             user_seed = seed)
+  
+  list_phenos <- generate_phenos(n, d,  user_seed = seed)
+  
+  dat_g <- generate_dependence(list_snps, list_phenos, ind_d0 = ind_d0,
+                               ind_p0 = ind_p0, vec_prob_sh = vec_prob_sh,
+                               family = "gaussian", max_tot_pve = max_tot_pve,
+                               block_phenos = TRUE, user_seed = seed)
+  
+  
+  mlocus <- function(fseed) {
+    
+    tau_vb_init <- 1 / apply(dat_g$phenos, 2, var)
+    
+    sig2_beta_vb_init <- 1 / rgamma(d, shape = 2, rate = 1 / tau_vb_init)
+    
+    gam_vb_init <-  matrix(rbeta(p * d, shape1 = 1, shape2 = d * (p - p0_av)/p0_av), nrow = p)
+    
+    
+    mu_beta_vb_init <- matrix(rnorm(n = p * d , mean=0, sd = 1), nrow = p)
+    
+    list_init0 <-  set_init(d,p, gam_vb = gam_vb_init, mu_beta_vb = mu_beta_vb_init, 
+                            sig2_beta_vb = sig2_beta_vb_init, tau_vb = tau_vb_init)
+    
+    vb_g <- locus(Y = dat_g$phenos, X=dat_g$snps, p0_av = p0_av, link = "identity", user_seed = fseed, list_init = list_init0, full_output = TRUE, anneal = NULL)
+    
+    return(list(locus = vb_g, gam_init = gam_vb_init,mu = mu_beta_vb_init, sig = sig2_beta_vb_init))
+  }
+  
+  mlocus_anneal <- function(fseed) {
+    
+    tau_vb_init <- 1 / apply(dat_g$phenos, 2, var)
+    
+    sig2_beta_vb_init <- 1 / rgamma(d, shape = 2, rate = 1 / tau_vb_init)
+    
+    gam_vb_init <-  matrix(rbeta(p * d, shape1 = 1, shape2 = d * (p - p0_av)/p0_av), nrow = p)
+    
+    
+    mu_beta_vb_init <- matrix(rnorm(n = p * d , mean=0, sd = 1), nrow = p)
+    
+    list_init0 <-  set_init(d,p, gam_vb = gam_vb_init, mu_beta_vb = mu_beta_vb_init, 
+                            sig2_beta_vb = sig2_beta_vb_init, tau_vb = tau_vb_init)
+    
+    t_anneal <- runif(1,10,1000)
+    
+    vb_g <- locus(Y = dat_g$phenos, X=dat_g$snps, p0_av = p0_av, link = "identity", user_seed = fseed, list_init = list_init0, full_output = TRUE, anneal = c(1,t_anneal,1))
+
+    return(list(locus = vb_g, gam_init = gam_vb_init,mu = mu_beta_vb_init, sig = sig2_beta_vb_init))
+  }
+  
+  
+  # MAC
+  m_vb_g <- mclapply(user_seed, mlocus, mc.cores = nb_cpus)
+  m_vb_g_a <- mclapply(user_seed, mlocus_anneal, mc.cores = nb_cpus)
+  
+  if(TRUE) {
+    
+    
+    out <- 0
+    lb_exp <- 0
+    elbo <- NULL
+    out_a <- 0
+    lb_exp_a <- 0
+    elbo_a <- NULL
+    
+    
+    if(TRUE){
+      for(i in c(1:length(user_seed))) {
+        out <- out + m_vb_g[[i]]$locus$gam_vb*exp(m_vb_g[[i]]$locus$lb_opt)
+        lb_exp <- lb_exp + exp(m_vb_g[[i]]$locus$lb_opt)
+        elbo <- c(elbo, exp(m_vb_g[[i]]$locus$lb_opt))
+      }
+ 
+      for(i in c(1:length(user_seed))) {
+        out_a <- out_a + m_vb_g_a[[i]]$locus$gam_vb*exp(m_vb_g_a[[i]]$locus$lb_opt)
+        lb_exp_a <- lb_exp_a + exp(m_vb_g_a[[i]]$locus$lb_opt)
+        elbo_a <- c(elbo_a, exp(m_vb_g_a[[i]]$locus$lb_opt))
+      }
+    }
+    
+    out <- out / lb_exp
+    out_a <- out_a / lb_exp_a
+    
+  }
+  
+  single_vb_g <-locus(Y = dat_g$phenos, X=dat_g$snps, p0_av = p0_av, link = "identity", user_seed = seed, verbose = FALSE, save_hyper=TRUE, save_init = TRUE,full_output=TRUE)
+  
+  if(TRUE) {
+
+    plot(out[1:50], main='Probabilities of link between a phenotype and SNPs, comparison between m_locus and annealing m_locus',type='h',lwd=3,lend=1, ylim = c(0,1))
+    points(out_a[1:50],lwd=1,lend=1,type="h",col="red")
+    points(ind_p0,out[ind_p0], col='black')
+    points(ind_p0, out_a[ind_p0], col = "red")
+
+    }
+
+}
+
+
+
+
--- R/rbugsBIS.R
+++ R/rbugsBIS.R
@@ -8,6 +8,7 @@ require(R2OpenBUGS)
 require(coda)
 require(lattice)
 require(nor1mix)
+require(plotly)
 
 a <- 1
 
@@ -89,15 +90,17 @@ for (k in sample(1:1e3,iter)){
                             sig2_beta_vb = sig2_beta_vb_init, tau_vb = tau_vb_init)
     
     vb_g <- locus(Y = dat_g$phenos, X=dat_g$snps, p0_av = p0_av, link = "identity", user_seed = fseed, list_init = list_init0, full_output = TRUE)
+    
     return(list(locus = vb_g, gam_init = gam_vb_init,mu = mu_beta_vb_init, sig = sig2_beta_vb_init))
   }
   
   
   # MAC
+  m_vb_g <- mclapply(user_seed, mlocus, mc.cores = nb_cpus)
   
   if(TRUE) {
     
-    m_vb_g <- mclapply(user_seed, mlocus, mc.cores = nb_cpus)
+   
     out <- 0
     lb_exp <- 0
     
@@ -178,8 +181,8 @@ for (k in sample(1:1e3,iter)){
     }
     
     if(FALSE){
-      mix1 <- norMix(c(single_vb_g$mu_beta_vb[1],0),sigma = c(single_vb_g$sig2_beta_vb,0.0001),w=c(single_vb_g$gam_vb[1],1-single_vb_g$gam_vb[1]))
-      mix2 <- norMix(c(single_vb_g$mu_beta_vb[2],0),sigma = c(single_vb_g$sig2_beta_vb,0.0001),w=c(single_vb_g$gam_vb[2],1-single_vb_g$gam_vb[2]))
+      mix1 <- norMix(c(single_vb_g$mu_beta_vb[1],0),sigma = c(single_vb_g$sig2_beta_vb,0.01),w=c(single_vb_g$gam_vb[1],1-single_vb_g$gam_vb[1]))
+      mix2 <- norMix(c(single_vb_g$mu_beta_vb[2],0),sigma = c(single_vb_g$sig2_beta_vb,0.01),w=c(single_vb_g$gam_vb[2],1-single_vb_g$gam_vb[2]))
       
       joint.density.plot(rnorMix(1e5,mix1),rnorMix(1e5,mix2))
       
@@ -188,14 +191,38 @@ for (k in sample(1:1e3,iter)){
     }
   
     if(TRUE){
-      m_mix1 <- norMix(mu=mu_m_locus_1,sigma=sig_m_locus,w=gam_m_locus_1)
-      m_mix2 <- norMix(mu=mu_m_locus_2,sigma=sig_m_locus,w=gam_m_locus_2)
+      m_mix1 <- norMix(mu=mu_m_locus_1,sigma=sig_m_locus,w=1/lb_exp*gam_m_locus_1)
+      m_mix2 <- norMix(mu=mu_m_locus_2,sigma=sig_m_locus,w=1/lb_exp*gam_m_locus_2)
       
+      #plot(m_mix1)
       
-      kill <- kde2d(rnorMix(1e5,m_mix_1),rnorMix(1e5,m_mix_2))
+      rmix1 <- rnorMix(1e5,m_mix1)
+      rmix2 <- rnorMix(1e5,m_mix2)
+      }
+    if(FALSE){
+      joint.density.plot(rmix1,rmix2,contour=T,color=F)
+      
+      kill <- kde2d(rmix1,rmix2)
       image(kill)
-      contour(kill,levels=c(0.1,0.3,0.5,0.7,0.9,1.1,1.3),method="simple",xlim=c(-2,2),ylim=c(-2,2))
+      #contour(kill,levels=c(0.1,0.3,0.5,0.7,0.9,1.1,1.3),method="simple",xlim=c(-2,2),ylim=c(-2,2))
+      contour(kill,method="simple",levels=c(1e-5,1e-3,1e-1,1))
+      plot_ly(x=rmix1, y=rmix2)
+      plot_ly(x=rmix1, y=rmix2,type="histogram2dcontour")
     }
+    if(TRUE){
+      
+      s <- subplot(
+        plot_ly(x = rmix1, type = "histogram"),
+        plotly_empty(),
+        plot_ly(x = rmix1, y = rmix2, type = "histogram2dcontour"),
+        plot_ly(y = rmix2, type = "histogram"),
+        nrows = 2, heights = c(0.2, 0.8), widths = c(0.8, 0.2), margin = 0,
+        shareX = TRUE, shareY = TRUE, titleX = FALSE, titleY = FALSE
+      )
+      p <- layout(s, showlegend = FALSE)
+      p
+      
+    q}
 }
 
 
